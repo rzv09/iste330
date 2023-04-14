cadd48f778e845376c7c2684adb9c32af9385f13
Raman Z <rzv090701@gmail.com>
2023-04-13 22:24:40 -0400

    minor changes in sql schema

--- mysql/Group5_Database.sql
+++ mysql/Group5_Database.sql
@@ -3,10 +3,10 @@ DROP DATABASE IF EXISTS CollegeConnection;
 CREATE DATABASE CollegeConnection;
 USE CollegeConnection;
 
-
 CREATE TABLE Faculty(
    facultyID    int PRIMARY KEY AUTO_INCREMENT,
-   name         varchar(45) NOT NULL,
+   firstName    varchar(45) NOT NULL,
+   lastName     varchar(45) NOT NULL,
    email        varchar(45) NOT NULL,
    password     varchar(50) NOT NULL,
    phone        varchar(16) NOT NULL,
@@ -25,8 +25,8 @@ CREATE TABLE Faculty_Abstract
     facultyID int,
     abstractID int,
     PRIMARY KEY (facultyID, abstractID),
-    FOREIGN KEY (facultyID) REFERENCES Faculty(facultyID),
-    FOREIGN KEY (abstractID) REFERENCES Abstract(abstractID)
+    FOREIGN KEY (facultyID) REFERENCES Faculty(facultyID) ON DELETE CASCADE ON UPDATE CASCADE,
+    FOREIGN KEY (abstractID) REFERENCES Abstract(abstractID) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE Faculty_Topic(
@@ -39,13 +39,14 @@ CREATE TABLE Faculty_Keyword
     facultyID int,
     keywordID int,
     PRIMARY KEY (facultyID, keywordID),
-    FOREIGN KEY (facultyID) REFERENCES Faculty(facultyID),
-    FOREIGN KEY (keywordID) REFERENCES Faculty_Topic(keyword_ID)
+    FOREIGN KEY (facultyID) REFERENCES Faculty(facultyID) ON DELETE CASCADE ON UPDATE CASCADE,
+    FOREIGN KEY (keywordID) REFERENCES Faculty_Topic(keyword_ID) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE Student(
     studentID    int PRIMARY KEY AUTO_INCREMENT,
-    name         varchar(45) NOT NULL,
+    firstName    varchar(45) NOT NULL,
+    lastName     varchar(45) NOT NULL,
     email        varchar(45) NOT NULL,
     password     varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
@@ -60,8 +61,8 @@ CREATE TABLE Student_Keyword
     studentID int,
     keywordID int,
     PRIMARY KEY (studentID, keywordID),
-    FOREIGN KEY (studentID) REFERENCES Faculty(facultyID),
-    FOREIGN KEY (keywordID) REFERENCES Student(studentID)
+    FOREIGN KEY (studentID) REFERENCES Faculty(facultyID) ON DELETE CASCADE ON UPDATE CASCADE,
+    FOREIGN KEY (keywordID) REFERENCES Student(studentID) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE Guest(
@@ -81,6 +82,6 @@ CREATE TABLE Guest_Keyword
     guestID int,
     keywordID int,
     PRIMARY KEY (guestID, keywordID),
-    FOREIGN KEY (guestID) REFERENCES Faculty(facultyID),
-    FOREIGN KEY (keywordID) REFERENCES Guest(guestID)
+    FOREIGN KEY (guestID) REFERENCES Faculty(facultyID) ON DELETE CASCADE ON UPDATE CASCADE,
+    FOREIGN KEY (keywordID) REFERENCES Guest(guestID) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
