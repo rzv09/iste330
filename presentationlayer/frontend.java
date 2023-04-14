606ba942b309f6b2b3fce1edc69d80632ccdcb03
Declan Naughton <djn5293@rit.edu>
2023-04-14 15:37:27 -0400

    Adding front end layer

---
+++ presentationlayer/frontend.java
@@ -0,0 +1,18 @@
+/**
+ * File: frontend.java
+ * Author: Declan Naughton
+ * A file which will handle all of the front end interaction.
+ */
+package presentationlayer;
+import datalayer.DataLayer;
+import java.io.*;
+import java.sql.*;
+
+
+public class frontend {
+    
+
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
